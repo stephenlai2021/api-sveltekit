678ed0f44aae51225f61c301f22bad57b710ceac
Stephen_Lai <stephenlai2021@gmail.com>
2023-01-20 23:40:56 +0800

    commit

--- src/routes/api/get-turn-credentials/+server.js
+++ src/routes/api/get-turn-credentials/+server.js
@@ -1,13 +1,13 @@
 import twilio from 'twilio'
-// import { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } from "$env/static/private";
+import { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } from "$env/static/private";
 
 export const GET = async () => {
-  const accountSid = "ACbd051822a344707fd90b94d91d284311"
-  const authToken = "9cc7c56e0ec2a450210b54aec9c4bcb7"
+  // const accountSid = "ACbd051822a344707fd90b94d91d284311"
+  // const authToken = "9cc7c56e0ec2a450210b54aec9c4bcb7"
   // const client = twilio(accountSid, authToken, { lazyloading: true })  
-  const client = twilio(accountSid, authToken)  
+  // const client = twilio(accountSid, authToken)  
 
-  // const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, { lazyloading: true })  
+  const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, { lazyloading: true })  
   try {
     const token = await client.tokens.create()
     console.log('twilio token: ', token)
